bd625313321991def4847a1954f246cdb14e28ef
chen391-practic <aa2805503274@gmail.com>
2021-12-23 23:29:25 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args){
+        String bot_name = "HelloGit";
+        int bot_year = 2021;
+        System.out.println("Hello! My name is " + bot_name + ".");
+        System.out.println("I was created in " + bot_year + ".");
+    }
+}
