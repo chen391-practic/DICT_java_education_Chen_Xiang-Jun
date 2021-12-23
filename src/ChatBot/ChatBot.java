8e4e8b928bac50eb2d4a5fd06e29f63e6850d832
chen391-practic <aa2805503274@gmail.com>
2021-12-23 23:32:06 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,10 +1,16 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args){
+        Scanner Scanner = new Scanner(System.in);
         String bot_name = "HelloGit";
         int bot_year = 2021;
         System.out.println("Hello! My name is " + bot_name + ".");
         System.out.println("I was created in " + bot_year + ".");
+        System.out.println("Please, remind your name: ");
+        String name = Scanner.nextLine();
+        System.out.println("What a great name you have, " +name +"!");
+
     }
 }
