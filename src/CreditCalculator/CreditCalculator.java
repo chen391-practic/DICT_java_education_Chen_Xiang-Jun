cc37629ea165059488a48affdc9c6412cefa12c8
chen391-practic <aa2805503274@gmail.com>
2021-12-24 10:45:43 +0200

    CreditCalculator 2-nd stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -1,11 +1,29 @@
 package CreditCalculator;
+import java.util.Scanner;
 
 public class CreditCalculator {
     public static void main(String[] args){
-        System.out.println("Loan principal: 1000\n" +
-                "Month 1: repaid 250\n" +
-                "Month 2: repaid 250\n" +
-                "Month 3: repaid 500\n" +
-                "The loan has been repaid!");
+        Scanner SCANNER = new Scanner(System.in);
+        System.out.println("Enter the loan principal:");
+        int PRINCIPAL = SCANNER.nextInt();
+        System.out.println(PRINCIPAL);
+        System.out.println("What do you want to calculate?\n" +
+                "type \"m\" – for number of monthly payments,\n" +
+                "type \"p\" – for the monthly payment:");
+        String CHOICE = SCANNER.nextLine();
+
+        if (CHOICE.equals("m")){
+            System.out.println("Enter the monthly payment:");
+            int MONTH = SCANNER.nextInt();
+            int SUMM = PRINCIPAL / MONTH;
+            System.out.println("It will take " + SUMM + " months to repay the loan");
+        }
+
+        if (CHOICE.equals("p")){
+            System.out.println("Enter the number payment:");
+            int payment = SCANNER.nextInt();
+            int sum2 = PRINCIPAL / payment;
+            System.out.println("Your monthly payment =" + sum2);
+        }
     }
 }
