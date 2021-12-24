59e6419b8afd07023dd539ca7f46da2eaea11db5
chen391-practic <aa2805503274@gmail.com>
2021-12-24 02:14:34 +0200

    TicTacToe 1-st stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args){
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
