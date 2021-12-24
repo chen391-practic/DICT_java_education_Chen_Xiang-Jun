2cc53b1456389042d062e7a3a0382392c1e8c1e7
chen391-practic <aa2805503274@gmail.com>
2021-12-24 10:51:14 +0200

    CreditCalculator 4-th stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -7,9 +7,9 @@ public class CreditCalculator {
     public static double A = 0, P = 0, n = 0, i = 0;
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner SCANNER = new Scanner(System.in);
         while(true){
-            Scanner SCANNER = new Scanner(System.in);
             System.out.println("What do you want to calculate? \n" +
                     "type \"n\" for number of monthly payments,\n" +
                     "type \"a\" for annuity monthly payment amount,\n" +
@@ -33,12 +33,22 @@ public class CreditCalculator {
                 else if(mode.equals("a") || mode.equals("A")){
                     writeData(true,  false,  false);
                     A = absoluteMonth(annualPayment(P, n, i));
-                    System.out.println("Your monthly payment = " + (int)A +"! ");
+                    System.out.println("Your monthly payment = " + (int)A + "! ");
                 }
                 else if(mode.equals("p") || mode.equals("P")){
                     writeData(false,  true, false);
                     P = (int)principalLoanAmount(A, n, i);
                     System.out.println("Your loan principal = " + (int)P +"! ");
+                }
+                else if(mode.equals("d") || mode.equals("D")){//test diff funk
+                    writeData(true,  false,  false);
+                    int overPayment = 0;
+                    for (int m = 1; m <= n; m++) {
+                        int D = absoluteMonth(Payment(P, n, i, m));
+                        System.out.println("Month " + m + ": payment is " + D);
+                        overPayment += D;
+                    }
+                    System.out.println("Overpayment = " + (int)(overPayment - P));
                 }
             }
             catch(Exception e){
@@ -51,61 +61,62 @@ public class CreditCalculator {
     private static void writeData(boolean _A, boolean _P, boolean _n){
         Scanner Scanner = new Scanner(System.in);
         if(!_P){
-            System.out.println("Enter the loan principal:");
+            System.out.println("Enter the loan principal: ");
             try {
                 P = Scanner.nextDouble();
             } catch (Exception e) {
-                System.out.println("Incorrect parameters!");
+                System.out.println("Incorrect parameters ");
             }
         }
         if(!_A){
-            System.out.println("Enter the monthly payment:");
+            System.out.println("Enter the monthly payment: ");
             try {
                 A = Scanner.nextDouble();
             } catch (Exception e) {
-                System.out.println("Incorrect parameter!");
+                System.out.println("Incorrect parameters ");
             }
         }
         if(!_n){
-            System.out.println("Enter the number of periods:");
+            System.out.println("Enter the number of periods: ");
             try {
                 n = Scanner.nextDouble();
             } catch (Exception e) {
-                System.out.println("Incorrect parameters!");
+                System.out.println("Incorrect parameters ");
             }
         }
-        System.out.println("Enter the loan interest:");
+        System.out.println("Enter the loan interest: ");
         try {
             i = Scanner.nextDouble();
         } catch (Exception e) {
-            System.out.println("Incorrect parameters!");
+            System.out.println("Incorrect parameters ");
         }
     }
 
     private static double annualPayment(double P, double n, double i){
-        i = PROCENT(i);
+        i = procent(i);
         return (P * ((i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1)));
     }
     private static double principalLoanAmount(double A, double n, double i){
-        i = PROCENT(i);
+        i = procent(i);
         return A / ((i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
     }
     private static double numberOfPayments(double A, double P, double i){
-        i = PROCENT(i);
-        return LOG(1 + i, (A / (A - (i * P))));
+        i = procent(i);
+        return log(1 + i, (A / (A - (i * P))));
     }
     private static double Payment(double P, double n, double i, double m){
+        i = procent(i);
         return (P / n) + (i * (P - ((P * (m - 1)) / n)));
     }
 
-    private static double LOG(double base, double x){
+    private static double log(double base, double x){
         return Math.log(x) / Math.log(base);
     }
-    private static double PROCENT(double value){
-        return (value / (1200));
+    private static double procent(double VALUE){
+        return (VALUE / (1200));
     }
 
-    private static int absoluteMonth(double MONTH) {
-        return (MONTH - (int) MONTH) > 0.0 ? (int) MONTH + 1 : (int) MONTH;
+    private static int absoluteMonth(double MONTH){
+        return (MONTH - (int)MONTH) > 0.0? (int)MONTH + 1: (int)MONTH;
     }
 }
