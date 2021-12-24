f8060b7202fc3521f58acf2c215f2b755b76a7f8
chen391-practic <aa2805503274@gmail.com>
2021-12-24 02:19:38 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,9 +1,18 @@
 package TicTacToe;
+import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args){
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O");
+        Scanner Scanner = new Scanner(System.in);
+        String User_Input = Scanner.nextLine();
+        System.out.println("--------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%s ", User_Input.charAt(3 * i + j));
+            }
+            System.out.println("|");
+        }
+        System.out.println("--------");
     }
 }
